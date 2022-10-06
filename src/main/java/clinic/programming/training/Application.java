35376478bb9ba919b59d8f38968315c4416123eb
Mithun M <mmithunsai@gmail.com>
2022-10-06 14:29:52 +0530

    Adds lang3 StringUtils dependency from maven central repo

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,11 +1,16 @@
 package clinic.programming.training;
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
     public Application() {
         System.out.println ("Inside Application");
+    }
+    public int countWords(String words){
+      String[] separateWords= StringUtils.split(words," ");
+      return (separateWords==null)?0:separateWords.length;
     }
 
     public void greet(){
@@ -22,5 +27,6 @@ public class Application {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
       app.greet();
+      System.out.println("The number of words: "+app.countWords("There are four words."));
     }
 }
